04c83f7d741ba71fe6f33fd660d9e91a384087c2
Will Colding <william.colding@gmail.com>
2022-05-18 14:04:57 -0400

    Improved dropdown functionality

--- Lua/SelectButton.lua
+++ Lua/SelectButton.lua
@@ -1,5 +1,7 @@
 function onValueChanged(key)
   if key == 'touch' and self.values.touch == true then
     self.parent.children['CameraSelect'].visible = true
+    self.parent.children['CameraColor'].color = Colors.black
+    self.parent.children['CameraLabel'].values.text = 'Select mix'
   end
 end
