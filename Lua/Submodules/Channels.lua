44e1124b9a50b87ae154a24727503941d91df95a
Will Colding <william.colding@gmail.com>
2022-05-20 14:12:57 -0400

    Factory function for channelData population

--- Lua/Submodules/Channels.lua
+++ Lua/Submodules/Channels.lua
@@ -1,32 +1,59 @@
 --Submodule.start('Channels.lua')
-local buttonColors =
-{
-  [0] = Colors.black,                    -- black (placeholder; we will hide the button if it is set to this)
-  [1] = Color.fromHexString('e72d2eff'), -- red
-  [2] = Color.fromHexString('18D200FF'), -- green
-  [3] = Color.fromHexString('FCFF0BFF'), -- yellow
-  [4] = Color.fromHexString('17B1FFFF'), -- blue
-  [5] = Color.fromHexString('ed27acff'), -- magenta
-  [6] = Color.fromHexString('2de0e7ff'), -- cyan
-  [7] = Colors.gray                      -- white
-}
 
-Ch01 = {}
-Ch01.name           = 'Red'
-Ch01.color          = Colors.red
-Ch01.channel        = 1
-Ch01.midiChannel    = 1
-Ch01.midiController = 21
+-- This remaps colors to better match the X32 scheme
+local buttonColors = {}
+buttonColors[0] = Colors.black                    -- black (placeholder; we will hide the button if it is set to this)
+buttonColors[1] = Color.fromHexString('e72d2eff') -- red
+buttonColors[2] = Color.fromHexString('18D200FF') -- green
+buttonColors[3] = Color.fromHexString('FCFF0BFF') -- yellow
+buttonColors[4] = Color.fromHexString('17B1FFFF') -- blue
+buttonColors[5] = Color.fromHexString('ed27acff') -- magenta
+buttonColors[6] = Color.fromHexString('2de0e7ff') -- cyan
+buttonColors[7] = Colors.gray                     -- white
 
-Ch02 = {}
-Ch02.name           = 'Blue'
-Ch02.color          = Colors.blue
-Ch02.channel        = 2
-Ch02.midiChannel    = 1
-Ch02.midiController = 41
-
+buttonColors.Black   = buttonColors[0]
+buttonColors.Red     = buttonColors[1]
+buttonColors.Green   = buttonColors[2]
+buttonColors.Yellow  = buttonColors[3]
+buttonColors.Blue    = buttonColors[4]
+buttonColors.Magenta = buttonColors[5]
+buttonColors.Cyan    = buttonColors[6]
+buttonColors.White   = buttonColors[7]
 
 local channelData = {}
-table.insert(channelData, Ch01)
-table.insert(channelData, Ch02)
+
+function AddChannel(name, color, channel, midiChannel, midiController)
+  temp = {}
+  temp.name           = name
+  temp.color          = color
+  temp.channel        = channel
+  temp.midiChannel    = midiChannel
+  temp.midiController = midiController
+  table.insert(channelData, temp)
+end
+
+AddChannel('Walt',   buttonColors.Red,   1,  1, 21)
+AddChannel('Jesse',  buttonColors.Blue,  2,  1, 41)
+
+AddChannel('Skyler', buttonColors.Green, 3,  1, 22)
+AddChannel('Hank',   buttonColors.Green, 4,  1, 23)
+AddChannel('Gus',    buttonColors.Green, 5,  1, 24)
+AddChannel('Mike',   buttonColors.Green, 6,  1, 25)
+
+AddChannel('Saul',   buttonColors.Green, 7,  1, 26)
+AddChannel('Tuco',   buttonColors.Green, 8,  1, 27)
+AddChannel('Hector', buttonColors.Green, 9,  1, 28)
+AddChannel('Kim',    buttonColors.Green, 10, 1, 42)
+
+AddChannel('Howard', buttonColors.Green, 11, 1, 43)
+AddChannel('Cliff',  buttonColors.Green, 12, 1, 44)
+AddChannel('Lalo',   buttonColors.Green, 13, 1, 45)
+AddChannel('Nacho',  buttonColors.Green, 14, 1, 46)
+
+AddChannel('Marie',  buttonColors.Green, 15, 1, 47)
+AddChannel('Chuck',  buttonColors.Green, 16, 1, 48)
+AddChannel('Lydia',  buttonColors.Green, 17, 1, 62)
+AddChannel('Todd',   buttonColors.Green, 18, 1, 63)
+
+AddChannel('Boom',   buttonColors.Cyan,  19, 1, 64)
 --Submodule.end()
